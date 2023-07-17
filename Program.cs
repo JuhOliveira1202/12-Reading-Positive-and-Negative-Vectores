1b12fd8fbf1d64eb0bde9aa4ea5ac97d03c4c349
Juliana Oliveira <134332673+JuhOliveira1202@users.noreply.github.com>
2023-07-17 13:17:55 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        //ler o vetor 
+
+        int n;
+
+        Console.Write("Digite aqui o número de vetores: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        int[] vec = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("vec ["+ i + "]: ");
+            vec[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        //identificar quais são os números negativos
+
+        int contNeg = 0;
+
+        for( int i = 0; i < n; i++)
+        {
+            if (vec[i] < 0)
+            {
+                contNeg++;
+            }
+            
+        }
+        Console.WriteLine("Os negativos são: " + contNeg);
+
+        //identificar os múltiplos de 5
+
+        int mult5 = 0;
+
+        for ( int i = 0; i < n; i++)
+        {
+            if (vec[i] % 5 == 0)
+            {
+                mult5++;
+            }
+        }
+        Console.WriteLine("Os números apresentados multiplos de 5 são: " + mult5);
+    }
+}
